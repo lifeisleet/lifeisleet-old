e2f308bbf061b1acc3f66402279203536608a1bc
Jesse B. Hannah <jesse@jbhannah.net>
2015-05-13 22:46:13 -0700

    Admin model RailsAdmin config

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,7 +1,32 @@
 class Admin < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :rememberable, :trackable, :validatable, :lockable
+  devise :database_authenticatable, :rememberable, :trackable, :validatable,
+    :lockable
 
   validates :name, presence: true, uniqueness: true
+
+  rails_admin do
+    list do
+      field :name
+      field :email
+      field :last_sign_in_at
+      field :locked_at
+    end
+
+    show do
+      field :name
+      field :email
+      field :last_sign_in_at
+      field :last_sign_in_ip
+      field :locked_at
+    end
+
+    edit do
+      field :name
+      field :email
+    end
+  end
+
+  def to_s
+    name
+  end
 end
