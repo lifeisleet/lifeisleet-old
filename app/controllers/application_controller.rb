ddf0764b0b232d31aa75681adf148380110e2df8
Jesse B. Hannah <jesse@jbhannah.net>
2015-05-19 00:33:26 -0700

    Only enable PaperTrail in RailsAdmin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def paper_trail_enabled_for_controller
+    controller_path.camelize.deconstantize == "RailsAdmin"
+  end
 end
